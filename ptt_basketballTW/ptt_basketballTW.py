8b45a3d33e5de4dc2cd8d0e2791aeca9e9aa7ad5
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-23 20:37:23 +0800

    Update and rename ptt_basketballTW.py to ptt_basketballTW/ptt_basketballTW.py

--- ptt_basketballTW/ptt_basketballTW.py
+++ ptt_basketballTW/ptt_basketballTW.py
@@ -1,10 +1,10 @@
+# 模仿使用者爬蟲
 import requests
 
 url = "https://www.ptt.cc/bbs/basketballTW/index.html"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+response = requests.get(url, headers=headers)
 
-response = requests.get(url)
-
-#print(response.text)
 if response.status_code == 200:
     with open('output.html', 'w', encoding='utf-8') as f:
         f.write(response.text)
