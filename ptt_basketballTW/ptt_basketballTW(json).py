1327c20f31598260dfbe7e12f7f2617b64dbea35
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-23 22:36:12 +0800

    Create ptt_basketballTW(json).py

---
+++ ptt_basketballTW/ptt_basketballTW(json).py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://www.ptt.cc/bbs/basketballTW/index.html"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+articles = soup.find_all("div", class_="r-ent")
+data_list = []
+
+for a in articles:
+    data = {}
+    title = a.find("div", class_="title")
+    if title and title.a:
+        title = title.a.text
+    else:
+        title = "沒有標題"
+    data["標題"] = title
+    
+    popular = a.find("div", class_="nrec")
+    if popular and popular.span:
+        popular = popular.span.text
+    else:
+        popular = "N/A"
+    data["人氣"] = popular
+    
+    date = a.find("div", class_="date")
+    if date:
+        date = date.text
+    else:
+        date = "N/A"
+    data["日期"] = date
+    data_list.append(data)
+with open ("ptt_basketball_data.json", "w", encoding="utf-8") as file:
+    json.dump(data_list, file, ensure_ascii=False, indent=4)
+print("資料已儲存為ptt_basketball.json")
