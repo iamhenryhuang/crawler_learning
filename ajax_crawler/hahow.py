c2dcf0d64de87737e3aef2f1aed9e0b2a04f27e8
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-24 08:58:57 +0800

    Update hahow.py

--- ajax_crawler/hahow.py
+++ ajax_crawler/hahow.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 
 url = "https://api.hahow.in/api/products/search?category=COURSE&filter=PUBLISHED&limit=24&page=0&sort=TRENDING"
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
 }
